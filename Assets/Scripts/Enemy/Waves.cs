d75291a869b56dbd8179589320ac86aa3e89d2a5
Maxim Tretyakov <scryptan@gmail.com>
2021-02-04 20:41:07 +0500

    Make some work with spawners

--- Assets/Scripts/Enemy/Waves.cs
+++ Assets/Scripts/Enemy/Waves.cs
@@ -8,23 +8,31 @@ using UnityEngine;
 namespace RogueLike.Enemy
 {
     [RequireComponent(typeof(Spawner))]
+    [RequireComponent(typeof(PortalSpawner))]
     public class Waves : MonoBehaviour, INotifiable
     {
-        [SerializeField] private int _enemyCountAtThisWawe = 5;
-        [SerializeField] private int _enemyIncrement = 5;
-        [SerializeField] private int _delayToSpawnEnemyInSeconds = 1;
-        [SerializeField] private int _delayToStartWaveInSeconds = 15;
-        [SerializeField] private TMP_Text _timerText;
+        [SerializeField] private int enemyCountAtThisWawe = 5;
+        [SerializeField] private int enemyIncrement = 5;
+        [SerializeField] private int delayToSpawnEnemyInSeconds = 1;
+        [SerializeField] private int delayToStartWaveInSeconds = 15;
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private Transform portalSpawnpoint;
 
         private Spawner _enemySpawner;
+        private PortalSpawner _portalSpawner;
         private int _currentEnemyCount;
-
+        private int _currentWave;
+        
         private void Awake()
         {
             _enemySpawner = GetComponent<Spawner>();
-            _currentEnemyCount = _enemyCountAtThisWawe;
-            if (_timerText == null)
-                throw new ConstraintException($"Has no text at {name}");
+            _portalSpawner = GetComponent<PortalSpawner>();
+            _currentEnemyCount = enemyCountAtThisWawe;
+            if (timerText == null)
+                throw new ConstraintException($"Has no {nameof(timerText)} at {name}");
+            if (portalSpawnpoint == null)
+                throw new ConstraintException($"Has no {nameof(portalSpawnpoint)} at {name}");
+            
             StartWave();
         }
 
@@ -33,26 +41,49 @@ namespace RogueLike.Enemy
             _currentEnemyCount--;
             if (_currentEnemyCount <= 0)
             {
-                _enemyCountAtThisWawe += _enemyIncrement;
-                StartWave();
+                enemyCountAtThisWawe += enemyIncrement;
+                WaveBehaviour();
             }
         }
 
         private async Task StartWave()
         {
+            _currentWave++;
             await DelayToStartWave();
-            _enemySpawner.SpawnEnemyWithDelay(_delayToSpawnEnemyInSeconds.Seconds(), _enemyCountAtThisWawe, this);
+            _enemySpawner.SpawnEnemyWithDelay(delayToSpawnEnemyInSeconds.Seconds(), enemyCountAtThisWawe, this);
         }
 
+        private void WaveBehaviour()
+        {
+            if (_currentWave != 0 && _currentWave % 15 == 0)
+            {
+                _portalSpawner.Spawn(portalSpawnpoint.position);                
+                return;
+            }
+
+            if (_currentWave != 0 && _currentWave % 10 == 0)
+            {
+                StartBossWave();
+                return;
+            }
+
+            StartWave();
+        }
+
+        private void StartBossWave()
+        {
+            
+        }
+        
         private async Task DelayToStartWave()
         {
-            for (int i = _delayToStartWaveInSeconds; i > 0; i--)
+            for (int i = delayToStartWaveInSeconds; i > 0; i--)
             {
-                _timerText.text = $"{i} seconds to start";
+                timerText.text = $"{i} seconds to start";
                 await Task.Delay(1.Seconds());
             }
 
-            _timerText.text = String.Empty;
+            timerText.text = String.Empty;
         }
     }
 }
---
+++ Assets/Scripts/PortalSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RogueLike
+{
+    public class PortalSpawner : MonoBehaviour
+    {
+        public void Spawn(Vector3 position)
+        {
+            
+        }
+    }
+}
